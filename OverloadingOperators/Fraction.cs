09988e88c074601e2e771c36c4c675064eae0318
Henry <henrycoderchannel@gmail.com>
2022-08-08 22:26:51 +0700

    [Init] Upload code

---
+++ OverloadingOperators/Fraction.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverloadingOperators
+{
+    public class Fraction
+    {
+        private readonly int num;
+        private readonly int den;
+
+        public Fraction (int numerator, int denominator)
+        {
+            if (denominator == 0) throw new Exception("Denominator can't be 0");
+            num = numerator;
+            den = denominator;
+        }
+
+        public static Fraction operator - (Fraction f)
+        {
+            return new Fraction(-f.num, f.den);
+        }
+
+        // Fraction + Int
+        public static Fraction operator + (Fraction f, int number)
+        {
+            return new Fraction(f.num + (number * f.den), f.den);
+        }
+
+        // Fraction + Fraction
+        public static Fraction operator + (Fraction f1, Fraction f2)
+        {
+            return new Fraction((f1.num * f2.den + f2.num * f1.den), f1.den * f2.den);
+        }
+
+        #region Couple Operators
+        public static bool operator == (Fraction f1, Fraction f2)
+        {
+            return f1.num == f2.num && f1.den == f2.den;
+        }
+
+        public static bool operator != (Fraction f1, Fraction f2)
+        {
+            return !(f1.num == f2.num && f1.den == f2.den);
+        }
+        #endregion
+
+        public static implicit operator Fraction (int number)
+        {
+            return new Fraction(number, 1);
+        }
+
+        public static explicit operator int (Fraction f)
+        {
+            if (f.den != 1) throw new Exception("CANNOT Convert");
+            return f.num;
+        }
+
+        public override string ToString()
+        {
+            return $"{num}/{den}";
+        }
+
+    }
+}
---
+++ OverloadingOperators/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OverloadingOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f = new Fraction(3, 4);
+            Fraction f2 = new Fraction(9, 2);
+
+            Console.WriteLine($"-f: {-f}");
+
+            Console.WriteLine($"{f} + {7} = {f + 7}");
+            
+            Console.WriteLine($"{f} + {f2} = {f + f2}");
+
+            Console.WriteLine($"{f} == {f2} : {f == f2}");
+
+            Console.WriteLine($"{f} != {f2} : {f != f2}");
+
+            // Implicitly convert Int -> Fraction
+            // ==> ALWAYS SUCCEED
+            Fraction f3 = 91;
+            // f3 = 9/1
+            Console.WriteLine($"Convert {91} to Fraction: {f3}");
+
+            // Explicitly convert Fraction -> Int
+            // ==> EXCEPTION if denominator != 1 (9/2, 8/3,...)
+            int x = (int)f3;
+            // x = 91 (because of 91/1 = 91)
+            Console.WriteLine($"Convert {f3} to Int: {x}");
+
+            // ==> EXCEPTION
+            //Console.WriteLine($"Convert {f2} to Int: {(int)f2}");   
+
+            Console.ReadLine();
+
+        }
+    }
+}
